db334e0332d5dceb0e2a5f7198286151f0144c63
Waris Gill <wgill18@ku.edu.tr>
2020-05-16 00:57:46 +0300

    Paper submitted. Starting new data processing for 3 months

--- new-alarm/alarms.py
+++ new-alarm/alarms.py
@@ -9,28 +9,7 @@ import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
-def findChatterings(alarms, chattering_timedelta_threshold=60.0, chattering_count_threshold=3):
-    chattering = {}
-    
-    alarms = [alarm for alarm in sorted(alarms, key=lambda arg: arg["StartTime"], reverse=False)]    
-    count = 0
-    
-    for i in range(len(alarms)):
-        t_prev = alarms[i]["StartTime"]
-        count = 0
-        for j in range(i+1, len(alarms)):
-            t_next = alarms[j]["StartTime"]    
-            
-            if timedelta.total_seconds(t_next - t_prev) > chattering_timedelta_threshold:
-                break
-                
-            count += 1
-#             print(time_delta, "count ++ ", count, t_prev, t_next)
-        
-#         if count > chattering_count_threshold:
-        chattering[t_prev] = {"own_index": i, "count": count}
-            
-    return chattering
+
 
 
 def frequencyOfAlarmsActivated(alarms, timediff=60):
