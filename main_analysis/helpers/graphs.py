de3343b99d04f9f5defbddf1e2dfbd64cbb372f7
waris <wgill18@ku.edu.tr>
2021-08-11 09:30:42 +0300

    aug 11

--- main_analysis/helpers/graphs.py
+++ main_analysis/helpers/graphs.py
@@ -13,8 +13,7 @@ def intersection_of_graphs(graphs, edge_filter=None):
     assert edge_filter <= len(graphs)
     assert edge_filter > 1
 
-    print(
-        f">> Ingore the Edge if its not appearing in atleast {edge_filter}  graphs")
+    # print(f">> Ingore the Edge if its not appearing in atleast {edge_filter}  graphs")
 
     edges = [edge for g in graphs for edge in g.edges]
     edges_dict = {edge: 0 for edge in edges}
@@ -45,16 +44,25 @@ def intersection_of_graphs(graphs, edge_filter=None):
 
     return intersect_G
 
-def filterEdges(g, edge_drop_factor):
-    g = nx.DiGraph(g)
-    remove_edges = []
-    for source, target, weight in g.edges.data("weight"):
-        if edge_drop_factor * weight < g.nodes[target]["count"]:
-            remove_edges.append((source, target))
+# def filterEdges(g, edge_drop_factors):
+#     # g = nx.DiGraph(g)
+#     remove_edges = []
+#     for source, target, weight in g.edges.data("weight"):
+#         alarm_count = g.nodes[target]["count"]
+#         action_count = g.nodes[source]["count"]
 
-    g.remove_edges_from(remove_edges)
-    g.remove_nodes_from(list(nx.isolates(g)))
-    return g
+#         action_alarms_ratio = alarm_count/action_count
+#         weight_alarm_ratio =  weight/alarm_count
+
+#         # if edge_drop_factor * weight < g.nodes[target]["count"]:
+#         if  action_alarms_ratio <  0.4:
+#             remove_edges.append((source, target))
+#         elif action_alarms_ratio <0.3 and weight_alarm_ratio < 0.8:
+#             remove_edges.append((source, target))
+
+#     g.remove_edges_from(remove_edges)
+#     g.remove_nodes_from(list(nx.isolates(g)))
+#     return g
 
 def getInDegree(g, n):
     return g.in_degree(n, weight="weight")
